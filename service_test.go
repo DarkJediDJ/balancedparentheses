a8df1ad1b1440d397898b253a40292a0b505aa71
DarkJediDJ <xintut@mail.ru>
2021-11-28 15:40:13 +0300

    fix(actions):edited some testing scripts

--- service_test.go
+++ service_test.go
@@ -1,32 +1,51 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func TestHealthCheckHandler(t *testing.T) {
-	req, err := http.NewRequest("GET", "generate/5", nil)
-	if err != nil {
-		fmt.Println("alkfk")
-		t.Fatal(err)
+var a App
+
+func TestGenerateHandler(t *testing.T) {
+	testServiceCases := []struct {
+		route string
+		want  int
+		name  string
+	}{
+		{"5", 5, "Standard parentheses string"},
+		{"1000", 1000, "Long parentheses string"},
+		{"1", 1, "Short parentheses string"},
+		{"0", 0, "Emptyy parentheses string"},
 	}
 
-	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(generate)
+	for _, tc := range testServiceCases {
+		t.Run(tc.name, func(t *testing.T) {
+			req, err := http.NewRequest("GET", "/generate/"+tc.route, nil)
+			if err != nil {
+				t.Fatal(err)
+			}
 
-	handler.ServeHTTP(rr, req)
+			response := httptest.NewRecorder()
 
-	if status := rr.Code; status != http.StatusOK {
-		t.Errorf("handler returned wrong status code: got %v want %v",
-			status, http.StatusOK)
-	}
-
-	expected := `{"alive": true}`
-	if rr.Body.String() != expected {
-		t.Errorf("handler returned unexpected body: got %v want %v",
-			rr.Body.String(), expected)
+			a.New()
+			a.Router.ServeHTTP(response, req)
+			assert.Equal(t, http.StatusOK, response.Code, "Expected response code %d. Got %d\n", http.StatusOK, response.Code)
+			data, err := ioutil.ReadAll(response.Body)
+			if err != nil {
+				t.Errorf("expected error to be nil got %v", err)
+			}
+			var lines Parentheses
+			err = json.Unmarshal(data, &lines)
+			if err != nil {
+				t.Errorf("expected error to be nil got %v", err)
+			}
+			assert.Equal(t, tc.want, len(lines.Line), "Expected line length %d. Got %d\n", tc.want, len(lines.Line))
+		})
 	}
 }
